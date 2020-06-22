120cfc09aea67130461e8afccdf3e8c7044101ad
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-06-22 15:02:25 -0600

    Configurar el menú de interfaz nuevo

--- app/src/main/java/com/example/contactos/Nuevo.kt
+++ app/src/main/java/com/example/contactos/Nuevo.kt
@@ -1,11 +1,33 @@
 package com.example.contactos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
 class Nuevo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nuevo)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
-}
+        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            menuInflater.inflate(R.menu.menu_nuevo, menu)
+            return super.onCreateOptionsMenu(menu)
+        }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            when(item?.itemId) {
+                R.id.iCrearNuevo ->{
+                   // Crear un nuevo elemenento de tipo Contacto
+                    return true
+                }
+                else ->{  return super.onOptionsItemSelected(item)}
+            }
+
+        }
+    }
