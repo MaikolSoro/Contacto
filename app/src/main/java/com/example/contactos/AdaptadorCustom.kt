eea6d4b7e26c4578554dfec4a73ee800edad96b9
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-06-21 18:52:32 -0600

    Impletación de ListView

---
+++ app/src/main/java/com/example/contactos/AdaptadorCustom.kt
@@ -0,0 +1,71 @@
+package com.example.contactos
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class AdaptadorCustom( var contexto: Context, items:ArrayList<Contacto>):BaseAdapter() {
+
+    // Almacenar los elementos que se van en el ListView
+
+    var items: ArrayList<Contacto>? = null
+
+    init {
+        this.items = items
+    }
+    //Asociar el rendirisado de los elementos
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        var viewHolder : ViewHolder? = null
+        var vista: View? = p1
+
+        if(vista == null) {
+            vista = LayoutInflater.from(contexto).inflate(R.layout.template_contacto, null)
+            viewHolder = ViewHolder(vista)
+            vista.tag = viewHolder
+        } else {
+            viewHolder = vista.tag as? ViewHolder
+        }
+
+        val item = getItem(p0) as Contacto
+
+        // Asiganción de valores a elementos gráficos
+        viewHolder?.nombre?.text = item.nombre
+        viewHolder?.empresa?.text = item.empresa
+        viewHolder?.foto?.setImageResource(item.foto)
+
+        return vista!!
+    }
+
+    override fun getItem(p0: Int): Any {
+
+        return this.items?.get(p0)!!
+    }
+
+    override fun getItemId(p0: Int): Long {
+       return p0.toLong()
+    }
+
+    override fun getCount(): Int {
+        //regresar el número de mi lista
+        return this.items?.count()!!
+    }
+
+    private  class  ViewHolder(vista:View) {
+        var nombre:TextView? = null
+        var foto: ImageView? = null
+        var empresa:TextView? = null
+
+        init{
+            nombre = vista.findViewById(R.id.tvNombre)
+            empresa = vista.findViewById(R.id.tvEmpresa)
+            foto = vista.findViewById(R.id.ivFoto)
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/contactos/MainActivity.kt
+++ app/src/main/java/com/example/contactos/MainActivity.kt
@@ -3,16 +3,26 @@ package com.example.contactos
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.widget.ListView
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+    var contactos:ArrayList<Contacto>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
-    }
+        contactos = ArrayList()
+        contactos?.add(Contacto("Maikol", "Soro", "Avantica", 21, 50.0F, "Santa Rita", "86223186", "maikolsoro.z1998@gmail.com", R.drawable.foto_01))
+
+        val lista = findViewById<ListView>(R.id.lista)
+        val adaptador = AdaptadorCustom( this,contactos!!)
+
+        lista.adapter = adaptador
+        }
 
 
 
