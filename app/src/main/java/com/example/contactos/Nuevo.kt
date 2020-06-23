639e510b0715bafbc5a171c221dad226d1db6669
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-06-23 09:38:47 -0600

    Cambiar la foto del perfil

--- app/src/main/java/com/example/contactos/Nuevo.kt
+++ app/src/main/java/com/example/contactos/Nuevo.kt
@@ -6,11 +6,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
 
 class Nuevo : AppCompatActivity() {
+
+    var fotoIndex:Int = 0
+    val fotos = arrayOf(R.drawable.foto_01, R.drawable.foto_02, R.drawable.foto_03, R.drawable.foto_04, R.drawable.foto_05, R.drawable.foto_06)
+    var foto:ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
 
 
@@ -22,6 +29,12 @@ class Nuevo : AppCompatActivity() {
 
         var actionBar = supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
+
+         foto = findViewById<ImageView>(R.id.ivFoto)
+
+        foto?.setOnClickListener {
+            seleccionarFoto()
+        }
     }
         override fun onCreateOptionsMenu(menu: Menu?): Boolean {
             menuInflater.inflate(R.menu.menu_nuevo, menu)
@@ -62,7 +75,7 @@ class Nuevo : AppCompatActivity() {
                     if(flag > 0){
                         Toast.makeText(this, "Rellena todos los campos", Toast.LENGTH_SHORT).show()
                     } else {
-                        MainActivity.agregarContacto(Contacto(campos.get(0), campos.get(1), campos.get(2),campos.get(3).toInt(),campos.get(4).toFloat(),campos.get(5), campos.get(6),campos.get(7   ),R.drawable.foto_03))
+                        MainActivity.agregarContacto(Contacto(campos.get(0), campos.get(1), campos.get(2),campos.get(3).toInt(),campos.get(4).toFloat(),campos.get(5), campos.get(6),campos.get(7),obtenerFoto(fotoIndex)))
                         finish()
                         Log.d("No ELEMENTOS", MainActivity.contactos?.count().toString())
                     }
@@ -72,5 +85,32 @@ class Nuevo : AppCompatActivity() {
                 else ->{  return super.onOptionsItemSelected(item)}
             }
 
+        }
+        fun seleccionarFoto(){
+            val builder = AlertDialog.Builder( this)
+            builder.setTitle("Selecciona imagen de perfil")
+
+            val adaptadorDialogo = ArrayAdapter<String>(this, android.R.layout.simple_selectable_list_item)
+            adaptadorDialogo.add("Foto 01")
+            adaptadorDialogo.add("Foto 02")
+            adaptadorDialogo.add("Foto 03")
+            adaptadorDialogo.add("Foto 04")
+            adaptadorDialogo.add("Foto 05")
+            adaptadorDialogo.add("Foto 06")
+            builder.setAdapter(adaptadorDialogo){
+                dialog, which->
+                fotoIndex = which
+                foto?.setImageResource(obtenerFoto(fotoIndex))
+
+            }
+            builder.setNegativeButton("Cancelar"){
+                dialog, which ->
+                dialog.dismiss()
+            }
+            builder.show()
+        }
+
+        fun obtenerFoto(index: Int ): Int{
+           return fotos.get(index)
         }
     }
